7674c990b6686c133cd57b8a7cb2a3b5b3de3eeb
hugo gomes <gothugo21@gmail.com>
2025-03-06 01:00:46 +0000

    IMPROVE: db connection.

--- sql/connect_db.py
+++ sql/connect_db.py
@@ -1,25 +1,50 @@
 from tortoise import Tortoise
 import config as AppConfig
+import asyncio
 from loguru import logger
 
-# Tortoise ORM Database URL
-DB_URL = f"mysql://{AppConfig.db_user}:{AppConfig.db_password}@{AppConfig.db_host}:{AppConfig.db_port}/{AppConfig.db_name}"
+db_config = {
+    'connections': {
+        'default': {
+            'engine': 'tortoise.backends.mysql',
+            'credentials': {
+                'host': AppConfig.db_host,
+                'port': AppConfig.db_port,
+                'user': AppConfig.db_user,
+                'password': AppConfig.db_password,
+                'database': AppConfig.db_name,
+            }
+        }
+    },
+    'apps': {
+        'models': {
+            'models': ['sql.models'],
+            'default_connection': 'default',
+        }
+    }
+}
 
 async def init_db():
-    """Initialize database connection."""
-    try:
-        logger.info("Connecting to the database...")
-        await Tortoise.init(
-            db_url=DB_URL,
-            modules={"models": ["sql.models"]}
-        )
-        await Tortoise.generate_schemas()  # Ensures tables exist
-        logger.success("Database connected successfully!")
-    except Exception as e:
-        logger.error(f"Database connection failed: {e}")
-        raise
+    retries = AppConfig.db_retry_connection
+    for attempt in range(1, retries + 1):
+        try:
+            logger.info(f"Initializing database connection (Attempt {attempt}/{retries})...")
+            await Tortoise.init(config=db_config)
+            logger.info("Attempting to generate schemas.")
+            await Tortoise.generate_schemas()
+            logger.success("Database connection initialized and schemas generated.")
+            break
+        except Exception as e:
+            logger.error(f"Attempt {attempt} failed with error: {e}")
+            if attempt < retries:
+                logger.info("Retrying in 5 seconds...")
+                await asyncio.sleep(5)
+            else:
+                logger.error("All attempts to initialize the database have failed.")
+                raise
 
 async def close_db():
-    """Close database connection."""
+    logger.info("Closing database connections...")
     await Tortoise.close_connections()
-    logger.info("Database connection closed.")
+    logger.info("Database connections closed.")
+
