1a6fbeb0aaf0623de92f84eb1f5c73b3ed078185
hugo gomes <gothugo21@gmail.com>
2025-03-06 23:54:10 +0000

    ADD: class to redis connection.

--- my_redis/connect_redis.py
+++ my_redis/connect_redis.py
@@ -2,20 +2,46 @@ import redis.asyncio as redis
 import config as AppConfig
 from utils.logger import logger
 
-redis_client = None  # Global variable to store Redis connection
+class RedisManager:
+    """Handles Redis connection, reconnection, and closure."""
+    # Set class level attributes
+    redis_url = AppConfig.redis_url
 
-# Connect to Redis
-async def init_redis():
-    """Initialize Redis connection and handle errors."""
-    global redis_client
-    try:
-        logger.info("Connecting to Redis...")
-        redis_client = await redis.from_url(AppConfig.redis_url, decode_responses=True)
-        if await redis_client.ping(): # test connection
-            logger.success("Connected to Redis!")
+    def __init__(self):
+        """Initialiaze Redis client as None."""
+        self.redis_client = None
+
+    # Connect to Redis
+    async def init_redis(self):
+        """Initialize Redis connection and handle errors."""
+        try:
+            logger.info("Connecting to Redis...")
+            self.redis_client = await redis.from_url(self.redis_url, decode_responses=True)
+            if await self.redis_client.ping(): # test connection
+                logger.success("✅ Connected to Redis!")
+            else:
+                logger.error("❌ Failed to connect to Redis!")
+                self.redis_client = None
+        except Exception as e:
+            logger.error(f"❌ Failed to connect to Redis: {e}")
+            self.redis_client = None  # Prevent crashes if Redis is unavailable
+
+    async def close_redis(self):
+        """Close Redis connection."""
+        if self.redis_client:
+            await self.redis_client.close()
+            logger.success("✅ Closed Redis connection.")
+            self.redis_client = None
         else:
-            logger.error("Failed to connect to Redis!")
-            redis_client = None
-    except Exception as e:
-        logger.error(f"Failed to connect to Redis: {e}")
-        redis_client = None  # Prevent crashes if Redis is unavailable
+            logger.warning("⚠️ Redis connection is already closed.")
+
+
+    async def check_redis_connection(self):
+        """Check if Redis connection is running. This is an utilitary function."""
+        if not self.redis_client:
+            logger.warning("⚠️ Redis connection lost. Attempting to reconnect...")
+            await self.init_redis()
+
+        if self.redis_client is not None:
+            logger.debug("✅ Redis connection is active.")
+            return True
