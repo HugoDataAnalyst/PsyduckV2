0d99ded8b0cbefe1bb2c0179281ca99aaae70ac3
hugo gomes <gothugo21@gmail.com>
2025-03-10 00:39:51 +0000

    REWORK: webhook_app.py to use global_state.

--- server_fastapi/webhook_app.py
+++ server_fastapi/webhook_app.py
@@ -3,14 +3,14 @@ from contextlib import asynccontextmanager
 from utils.logger import logger
 from utils.koji_geofences import KojiGeofences
 from server_fastapi.routes import webhook_router
+from server_fastapi import global_state
 
-geofences = None
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global geofences
-    geofences = await KojiGeofences(3600).get_cached_geofences()
-    if not geofences:
+    global_state.geofences = await KojiGeofences(3600).get_cached_geofences()
+    if not global_state.geofences:
         logger.warning("⚠️ No geofences available at startup.")
     yield
     logger.info("Shutting down FastAPI application.")
@@ -18,4 +18,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 # Include the webhook router
-app.include_router(webhook_router)
+app.include_router(webhook_router.router)
