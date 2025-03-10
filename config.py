b85f5784db9722fae2e6a3c61de70749e13f9f48
hugo gomes <gothugo21@gmail.com>
2025-03-10 22:29:22 +0000

    REMOVE: total retention values.

--- config.py
+++ config.py
@@ -82,9 +82,7 @@ redis_db = get_env_int("REDIS_DB", 1)
 # Build Redis url connection
 redis_url = f"redis://:{redis_encoded_password}@{redis_host}:{redis_server_port}/{redis_db}"
 # Redis retention settings
-total_pokemon_retention_ms      = retention_ms(config.get("retention_hours", {}).get("total_pokemon", 720))
 timeseries_pokemon_retention_ms = retention_ms(config.get("retention_hours", {}).get("timeseries_pokemon", 720))
-tth_pokemon_retention_ms        = retention_ms(config.get("retention_hours", {}).get("tth_pokemon", 720))
 tth_timeseries_retention_ms     = retention_ms(config.get("retention_hours", {}).get("tth_timeseries_pokemon", 720))
 
 # Log Level
