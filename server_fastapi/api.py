15d7775a107922e65aa016ea9aeca7220b033ee4
hugo gomes <gothugo21@gmail.com>
2025-03-10 00:38:37 +0000

    WIP: api to be the actual API later on.

--- server_fastapi/api.py
+++ server_fastapi/api.py
@@ -3,23 +3,25 @@ from contextlib import asynccontextmanager
 from utils.logger import logger
 from utils.koji_geofences import KojiGeofences
 from server_fastapi.routes import webhook_router
+from server_fastapi import global_state
 
 """
 Actual API to retrieve data from the server.
 """
 
-geofences = None
+#geofences = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global geofences
-    geofences = await KojiGeofences(3600).get_cached_geofences()
-    if not geofences:
-        logger.warning("⚠️ No geofences available at startup.")
+    logger.success("✅ FastAPI application started.")
+    #global geofences
+    #geofences = await KojiGeofences(3600).get_cached_geofences()
+    #if not geofences:
+    #    logger.warning("⚠️ No geofences available at startup.")
     yield
-    logger.info("Shutting down FastAPI application.")
+    logger.info("Shutting down DATA Retriever FastAPI application.")
 
 app = FastAPI(lifespan=lifespan)
 
 # Include the webhook router
-app.include_router(webhook_router)
+#app.include_router(webhook_router)
