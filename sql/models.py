b9a64273c871f405b10c8815a17fe7b256d16bf5
hugo gomes <gothugo21@gmail.com>
2025-03-14 20:24:04 +0000

    ADD: pokestops/quests/invasions. Change forms to varchar.

--- sql/models.py
+++ sql/models.py
@@ -1,5 +1,6 @@
 from calendar import month
 from enum import unique
+from unittest.util import _MAX_LENGTH
 from tortoise.models import Model
 from tortoise import fields
 import config as AppConfig
@@ -22,6 +23,17 @@ class Spawnpoint(Model):
     class Meta:
         table = "spawnpoints"
 
+class Pokestops(Model):
+    """Stores pokestop information"""
+    id = fields.BigIntField(pk=True)
+    pokestop = fields.CharField(max_length=50, unique=True)
+    pokestop_name = fields.CharField(max_length=255)
+    latitude = fields.FloatField()
+    longitude = fields.FloatField()
+
+    class Meta:
+        table = "pokestops"
+
 class Gyms(Model):
     """Stores gym information."""
     id = fields.BigIntField(pk=True)
@@ -33,13 +45,62 @@ class Gyms(Model):
     class Meta:
         table = "gyms"
 
+class AggregatedQuests(Model):
+    """Stores aggregated quest data per pokestop, monthly"""
+    id = fields.BigIntField(pk=True)
+    pokestop = fields.ForeignKeyField("models.Pokestops", related_name="aggregated_stats")
+    ar_type = fields.SmallIntField()
+    normal_type = fields.SmallIntField()
+    reward_ar_type = fields.SmallIntField()
+    reward_normal_type = fields.SmallIntField()
+    reward_ar_item_id = fields.SmallIntField()
+    reward_ar_item_amount = fields.SmallIntField()
+    reward_normal_item_id = fields.SmallIntField()
+    reward_normal_item_amount = fields.SmallIntField()
+    reward_ar_poke_id = fields.SmallIntField()
+    reward_ar_poke_form = fields.CharField(max_length=15)
+    reward_normal_poke_id = fields.SmallIntField()
+    reward_normal_poke_form = fields.CharField(max_length=15)
+    area = fields.ForeignKeyField("models.Areas", related_name="aggregated_stats")
+    month_year = fields.SmallIntField()  # Format: YYMM (2503 for March 2025)
+    total_count = fields.IntField(default=0)
+
+    class Meta:
+        table = "aggregated_quests"
+        unique_together = (
+            "pokestop", "ar_type", "normal_type",
+            "reward_ar_type", "reward_normal_type",
+            "reward_ar_item_id", "reward_ar_item_amount", "reward_normal_item_id",
+            "reward_normal_item_amount", "reward_ar_poke_id", "reward_ar_poke_form",
+            "reward_normal_poke_id", "reward_normal_poke_form", "area", "month_year",
+        )
+
+class AggreagatedInvasions(Model):
+    """Stores aggregated invasion data per gym, monthly."""
+    id = fields.BigIntField(pk=True)
+    pokestop = fields.ForeignKeyField("models.Pokestops", related_name="aggregated_stats")
+    display_type = fields.SmallIntField()
+    character = fields.SmallIntField()
+    grunt = fields.SmallIntField()
+    confirmed = fields.SmallIntField()
+    area = fields.ForeignKeyField("models.Areas", related_name="aggregated_stats")
+    month_year = fields.SmallIntField()  # Format: YYMM (2503 for March 2025)
+    total_count = fields.IntField(default=0)
+
+    class Meta:
+        table = "aggregated_invasions"
+        unique_together = (
+            "pokestop", "display_type", "character",
+            "grunt", "confirmed", "area", "month_year"
+        )
+
 class AggregatedRaids(Model):
     """Stores aggregated raid data per gym, monthly."""
     id = fields.BigIntField(pk=True)
     gym = fields.ForeignKeyField("models.Gyms", related_name="aggregated_stats")
     raid_pokemon = fields.SmallIntField()
     raid_level = fields.SmallIntField()
-    raid_form = fields.SmallIntField(default=0)
+    raid_form = fields.CharField(max_length=15)
     raid_team = fields.SmallIntField(default=0)
     raid_costume = fields.SmallIntField(default=0)
     raid_is_exclusive = fields.SmallIntField(default=0)
@@ -51,7 +112,10 @@ class AggregatedRaids(Model):
     class Meta:
         table = "aggregated_raids"
         unique_together = (
-            "gym", "raid_pokemon", "raid_level", "raid_form", "raid_team", "raid_costume", "raid_is_exclusive", "raid_ex_raid_eligible", "area", "month_year"
+            "gym", "raid_pokemon", "raid_level",
+            "raid_form", "raid_team", "raid_costume",
+            "raid_is_exclusive", "raid_ex_raid_eligible",
+            "area", "month_year"
         )
 
 class AggregatedPokemonIVMonthly(Model):
@@ -59,7 +123,7 @@ class AggregatedPokemonIVMonthly(Model):
     id = fields.BigIntField(pk=True)
     spawnpoint = fields.ForeignKeyField("models.Spawnpoint", related_name="aggregated_stats")
     pokemon_id = fields.SmallIntField()
-    form = fields.SmallIntField(default=0)
+    form = fields.CharField(max_length=15)
     iv = fields.SmallIntField()
     area = fields.ForeignKeyField("models.AreaNames", related_name="aggregated_stats")
     month_year = fields.SmallIntField()  # Format: YYMM (2503 for March 2025)
@@ -68,7 +132,8 @@ class AggregatedPokemonIVMonthly(Model):
     class Meta:
         table = "aggregated_pokemon_iv_monthly"
         unique_together = (
-            "spawnpoint", "pokemon_id", "form", "iv", "area", "month_year"
+            "spawnpoint", "pokemon_id", "form",
+            "iv", "area", "month_year"
         )
 
 class ShinyUsernameRates(Model):
@@ -76,7 +141,7 @@ class ShinyUsernameRates(Model):
     id = fields.BigIntField(pk=True)
     username = fields.CharField(max_length=255)
     pokemon_id = fields.SmallIntField()
-    form = fields.SmallIntField(default=0)
+    form = fields.CharField(max_length=15)
     shiny = fields.SmallIntField(default=0)  # 0 = Not Shiny, 1 = Shiny
     area = fields.ForeignKeyField("models.AreaNames", related_name="shiny_username_rates")
     month_year = fields.SmallIntField()  # Format: YYMM (2503 for March 2025)
@@ -85,7 +150,8 @@ class ShinyUsernameRates(Model):
     class Meta:
         table = "shiny_username_rates"
         unique_together = (
-            "username", "pokemon_id", "form", "shiny", "area", "month_year"
+            "username", "pokemon_id", "form",
+            "shiny", "area", "month_year"
         )
 
 # Optional: Register models for Tortoise
