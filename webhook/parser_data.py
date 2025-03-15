4bb47b5fc55aac315f58d26350dad2fd48b01e08
hugo gomes <gothugo21@gmail.com>
2025-03-15 23:15:20 +0000

    FIX: logs + add new pokemon_tth_hourly_counterseries.py

--- webhook/parser_data.py
+++ webhook/parser_data.py
@@ -6,6 +6,7 @@ from my_redis.queries.pokemons import (
     pokemon_hourly_counterseries,
     pokemon_tth_counterseries,
     pokemon_tth_timeseries,
+    pokemon_tth_hourly_counterseries,
     pokemon_weather_iv_counterseries
 )
 from my_redis.queries.raids import (
@@ -58,6 +59,7 @@ async def process_pokemon_data(filtered_data):
             pokemon_hourly_counterseries_update = await pokemon_hourly_counterseries.update_pokemon_hourly_counter(filtered_data, pipe)
             pokemon_tth_timeseries_update = await pokemon_tth_timeseries.add_tth_timeseries_pokemon_event(filtered_data, pipe)
             pokemon_tth_counterseries_update = await pokemon_tth_counterseries.update_tth_pokemon_counter(filtered_data, pipe)
+            pokemon_tth_hourly_counterseries_update = await pokemon_tth_hourly_counterseries.update_tth_pokemon_hourly_counter(filtered_data, pipe)
             pokemon_weather_counterseries_update = await pokemon_weather_iv_counterseries.update_pokemon_weather_iv(filtered_data, pipe)
 
             # Execute all Redis commands in a single batch
@@ -83,10 +85,11 @@ async def process_pokemon_data(filtered_data):
             f"Area: {filtered_data['area_name']}\n"
             "Updates:\n"
             f"  - Timeseries Total: {json.dumps(pokemon_timeseries_update, indent=2)}\n"
-            f"  - Counter Total: {json.dumps(pokemon_counterseries_update, indent=2)}\n"
+            f"  - Counter Weekly Total: {json.dumps(pokemon_counterseries_update, indent=2)}\n"
             f"  - Counter Hourly Total: {json.dumps(pokemon_hourly_counterseries_update, indent=2)}\n"
             f"  - TTH Timeseries: {json.dumps(pokemon_tth_timeseries_update, indent=2)}\n"
-            f"  - TTH Counter: {json.dumps(pokemon_tth_counterseries_update, indent=2)}\n"
+            f"  - TTH Weekly Counter: {json.dumps(pokemon_tth_counterseries_update, indent=2)}\n"
+            f"  - TTH Hourly Counter: {json.dumps(pokemon_tth_hourly_counterseries_update, indent=2)}\n"
             f"  - Counter Weather: {json.dumps(pokemon_weather_counterseries_update, indent=2)}\n"
         )
 
@@ -136,8 +139,8 @@ async def process_raid_data(filtered_data):
             f"Area: {filtered_data['area_name']}\n"
             "Updates:\n"
             f"  - Raid Timeseries Total: {json.dumps(raid_timeseries_update, indent=2)}\n"
-            f"  - Raid Counter Total: {json.dumps(raid_counterseries_update, indent=2)}\n"
-            f"  - Raid Counter Hourly Total: {json.dumps(raid_hourly_counterseries_update, indent=2)}\n"
+            f"  - Raid Weekly Counter Total: {json.dumps(raid_counterseries_update, indent=2)}\n"
+            f"  - Raid Hourly Counter Total: {json.dumps(raid_hourly_counterseries_update, indent=2)}\n"
         )
 
         logger.debug(f"✅ Processed Raid {filtered_data['raid_pokemon']} in area {filtered_data['area_name']} - Updates: {structured_result}")
@@ -185,8 +188,8 @@ async def process_quest_data(filtered_data):
             f"Area: {filtered_data['area_name']}\n"
             "Updates:\n"
             f"  - Quest Timeseries Total: {json.dumps(quest_timeseries_update, indent=2)}\n"
-            f"  - Quest Counter Total: {json.dumps(quest_counterseries_update, indent=2)}\n"
-            f"  - Quest Counter Hourly Total: {json.dumps(quest_hourly_counterseries_update, indent=2)}\n"
+            f"  - Quest Weekly Counter Total: {json.dumps(quest_counterseries_update, indent=2)}\n"
+            f"  - Quest Hourly Counter Total: {json.dumps(quest_hourly_counterseries_update, indent=2)}\n"
         )
 
         logger.debug(f"✅ Processed Quest {filtered_data['with_ar']} in area {filtered_data['area_name']} - Updates: {structured_result}")
@@ -235,8 +238,8 @@ async def process_invasion_data(filtered_data):
             f"Area: {filtered_data['area_name']}\n"
             "Updates:\n"
             f"  - Invasion Timeseries Total: {json.dumps(invasion_timeseries_update, indent=2)}\n"
-            f"  - Invasion Counter Total: {json.dumps(invasion_counterseries_update, indent=2)}\n"
-            f"  - Invasion Counter Hourly Total: {json.dumps(invasion_hourly_counterseries_update, indent=2)}\n"
+            f"  - Invasion Weekly Counter Total: {json.dumps(invasion_counterseries_update, indent=2)}\n"
+            f"  - Invasion Hourly Counter Total: {json.dumps(invasion_hourly_counterseries_update, indent=2)}\n"
         )
 
         logger.debug(f"✅ Processed Invasion {filtered_data['invasion_type']} in area {filtered_data['area_name']} - Updates: {structured_result}")
