fd4e36799fa013c076bd1c5758856d185153ed8f
hugo gomes <gothugo21@gmail.com>
2025-03-17 17:22:54 +0000

    MOVE: secure_api into server_fastapi utils folder. ADD: details for FastAPI customisation.

---
+++ server_fastapi/utils/details.py
@@ -0,0 +1,18 @@
+TITLE = "PsyduckV2"
+
+DESCRIPTION = """
+
+## PsyduckV2 is a Data Pipeline project developed and maintained by [Hugo Gomes](https://github.com/HugoDataAnalyst).
+
+This API provides endpoints for processing webhooks and retrieving Redis/SQL data.
+For support or further information, please contact: hugogomes@databyhugo.com.
+"""
+
+VERSION = "0.0.1"
+
+TAGS_METADATA = [
+    {
+        "name": "Total Hourly Pokémon",
+        "description": "Obtain Total Hourly Pokémon Data from Redis."
+    },
+]
